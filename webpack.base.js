64d87f524eab86e75533154149c5d3a9cf15d1b4
hefeng6500 <2443992009@qq.com>
2020-03-14 21:52:10 +0800

    chore: 添加自动填充css前缀, cdn分离依赖包,开发环境生产环境分开配置

---
+++ webpack.base.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [
+        'babel-loader',
+        'eslint-loader'
+      ]
+    },
+    {
+      test: /\.css$/i,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            esModule: true
+          }
+        },
+        'css-loader',
+        'postcss-loader'
+      ]
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            esModule: true
+          }
+        },
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            // Prefer `dart-sass`
+            implementation: require('sass')
+          }
+        },
+        'postcss-loader'
+      ]
+    },
+    {
+      test: /\.(ttf|otf|ttc|eot|woff|woff2|font|fon)$/,
+      use: [
+        'file-loader'
+      ]
+    },
+    {
+      test: /\.(jpg|jpeg|png|gif|svg)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }
+      ]
+    }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false // Enable to remove warnings about conflicting order
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
