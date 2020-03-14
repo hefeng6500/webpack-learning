e2e6359fbf72d79e71cc1841025334efcaf7e55e
hefeng6500 <2443992009@qq.com>
2020-03-14 18:25:16 +0800

    add eslint config closes #1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import helloworld from './helloworld.js'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-document.write(helloworld())
+import './index.scss';
+import logo from './images/webpack.svg';
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="searhText">helloworld</h1>
+        <img alt="" className="imgStyle" src={logo} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Index />,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
-  entry: {
-    index: './src/index.js',
-    search: './src/search.js'
-  },
+  entry: './src/index.js',
   output: {
     filename: '[name].js',
     path: path.join(__dirname, 'dist')
@@ -18,50 +16,50 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [
+        'babel-loader',
+        'eslint-loader'
+      ]
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              // Prefer `dart-sass`
-              implementation: require('sass'),
-            },
+            // Prefer `dart-sass`
+            implementation: require('sass'),
           },
-        ],
-      },
-      {
-        test: /\.(ttf|otf|ttc|eot|woff|woff2|font|fon)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
+        },
+      ],
+    },
+    {
+      test: /\.(ttf|otf|ttc|eot|woff|woff2|font|fon)$/,
+      use: [
+        'file-loader'
+      ]
+    },
+    {
+      test: /\.(jpg|jpeg|png|gif|svg)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
           },
-        ],
-      }
+        },
+      ],
+    }
     ]
   },
   plugins: [
-
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
   ]
 }
